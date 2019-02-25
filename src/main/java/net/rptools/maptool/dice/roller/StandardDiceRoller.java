5f9100d31b66bfa257d1de1f2174f12cac5f6324
Craig Wisniewski <craig.wisniewski@gmail.com>
2019-02-25 22:36:11 +1030

    Added DiceUtil and refactor StandardDiceRoller to use it

--- src/main/java/net/rptools/maptool/dice/roller/StandardDiceRoller.java
+++ src/main/java/net/rptools/maptool/dice/roller/StandardDiceRoller.java
@@ -4,32 +4,19 @@ import net.rptools.maptool.dice.result.DiceExprResult;
 import net.rptools.maptool.dice.result.DiceRolls;
 import net.rptools.maptool.dice.result.DieRoll;
 
-import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 
-@DiceRollerDefinition(name="Standard Dice Roller", patterns = { "d", "D" }, description = "Just the Standard Dice Roller")
 /**
  * The standard dice roller.
  */
+@DiceRollerDefinition(name="Standard Dice Roller", patterns = { "d", "D" }, description = "Just the Standard Dice Roller")
 public class StandardDiceRoller implements DiceRoller {
-
-    /** {@link Random} class used to perform rolls. */
-    private static final Random random = new SecureRandom();
 
 
     @Override
     public DiceExprResult roll(String pattern, int numDice, int numSides) {
-        //final List<Integer> rolls = new LinkedList<>();
-        //random.ints(numDice, 1, numSides).forEach(rolls::add);
-        int[] rolls;
-        if (numSides == 1) {
-            rolls = new int[numDice];
-            Arrays.fill(rolls, 1);
-        } else {
-            rolls = random.ints(numDice, 1, numSides + 1).toArray();
-        }
+        int[] rolls = DiceUtil.getInstance().randomDiceRollsArray(numDice, numSides);
+
 
         int sum = 0;
 
